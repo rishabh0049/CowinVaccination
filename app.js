1cb95bbf06177d7da7159a7783bd8ba0091d69a1
NitinKumar101 <nitinkumar12577@gmail.com>
2023-02-19 13:35:14 +0530

    commit slot confiramtion
    Co-authored-by: Rishabh Saini <rishabh0049@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-var express=require("express");
-var bodyParser=require("body-parser");
+var express = require("express");
+var bodyParser = require("body-parser");
 
 const mongoose = require('mongoose');
 
 const { defaultMaxListeners } = require("events");
+const { ftruncateSync } = require("fs");
 mongoose.connect('mongodb://localhost:27017/cowinvaccination');
-var db=mongoose.connection;
+var db = mongoose.connection;
 db.on('error', console.log.bind(console, "connection error"));
-db.once('open', function(callback){
+db.once('open', function (callback) {
 	console.log("connection succeeded");
 })
 
-var app=express()
+var app = express()
 
 let userId;
 
@@ -21,125 +22,192 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
-app.post('/sign_up', function(req,res){
+app.post('/sign_up', function (req, res) {
 	var name = req.body.name;
-    var age = req.body.age;
-    var pincode = req.body.pincode;
+	var age = req.body.age;
+	var pincode = req.body.pincode;
 	var pass = req.body.password;
-    var aadhar = req.body.aadhar;
-	var phone =req.body.phone;
+	var aadhar = req.body.aadhar;
+	var phone = req.body.phone;
 
 	var data = {
 		"name": name,
-		"age":age,
-        "pincode":pincode,
-        "aadhar":aadhar,
-		"password":pass,
-		"phone":phone
+		"age": age,
+		"pincode": pincode,
+		"aadhar": aadhar,
+		"password": pass,
+		"phone": phone
 	}
-db.collection('users').insertOne(data,function(err, collection){
+	db.collection('users').insertOne(data, function (err, collection) {
 		if (err) throw err;
 		console.log("Record inserted Successfully");
-			
+
 	});
-		
+
 	return res.redirect('login.html');
 })
 
 
-app.post('/bookslot', function(req,res){
+app.post('/bookslot', function (req, res) {
 	console.log(req.body);
 	var firstDoze = req.body.firstDoze;
-    var secondDoze = req.body.secondDoze;
-    var timeSlot = req.body.timeSlot;
-	let first,second;
-    if(firstDoze == "on"){
+	var secondDoze = req.body.secondDoze;
+	var timeSlot = req.body.timeSlot;
+	let first, second;
+	if (firstDoze == "on") {
 		first = true
-	}else{
-		first=false
+	} else {
+		first = false
 	}
-    if(secondDoze == "on"){
+	if (secondDoze == "on") {
 		second = true
-	}else{
-		second=false
+	} else {
+		second = false
 	}
 
-	var data = {
-		"userId": userId,
-		"firstDoze":first,
-        "secondDose":second,  
-	}
-db.collection('vaccinedozes').insertOne(data,function(err, collection){
-		if (err) throw err;
-		console.log("Record inserted Successfully");
-		// alert("Succesfully Booked Slot")
-
-
-		db.collection('vaccine').updateOne({timeSlot:timeSlot},{$inc:{count:-1}},function(err, collection){
+	
+	console.log("before if")
+	// if(first==true){
+		console.log("one")
+		db.collection("vaccinedozes").findOne({ userId: userId }, function (err, result) {
 			if (err) throw err;
-			console.log("Record inserted Successfully");
-			// alert("Succesfully Booked Slot")
-				
-		});
-
-			
-	});
-
-
-
-		
-	return res.redirect('bookslot.html');
-
-
-
-})
-
-
-
-app.get("/login",(req,res)=>
-{
-	res.redirect("login.html");
-})
-
-app.post("/login", async(req,res)=>
-{
-	console.log("Entry 1");
-	
-    
-		try{
-			const phone = req.body.phone;
-			const password = req.body.password;
-			const userPhoneNumber = await db.collection("users").findOne({phone:phone});
-	
-			console.log(phone,password,userPhoneNumber);
-	
-			if(userPhoneNumber.password === password)
-			{
-				userId = phone;
-				res.send("login succesfully");
-			}
-			else{
-				res.send("Inavlid Login Details");
+			console.log(result)
+		if(result){
+			console.log("HI")
+			console.log(second);
+			if(second==true){
+				console.log("ttwwoo")
+				db.collection('vaccinedozes').updateOne({userId:userId},{$set:{ firstDoze: true,secondDose:second }},function (err, collection) {
+					if (err) throw err;
+					console.log("Updated Successfully");
+							
+					db.collection('vaccine').updateOne({ timeSlot: timeSlot }, { $inc: { count: -1 } }, function (err, collection) {
+						if (err) throw err;
+						console.log("Record inserted Successfully");
+						
+						
+					});
+				});
 			}
 
 		}
-     catch(err)
-	 {
-		console.log(err);
-	 }
+		
+		else{
+			var data = {
+				"userId": userId,
+				"firstDoze": first,
+				"secondDose": false,
+			}
+			db.collection('vaccinedozes').insertOne(data, function (err, collection) {
+				if (err) throw err;
+				console.log("Record inserted Successfully");
+				
+				
+				
+				db.collection('vaccine').updateOne({ timeSlot: timeSlot }, { $inc: { count: -1 } }, function (err, collection) {
+					if (err) throw err;
+					console.log("Record inserted Successfully");
+					
+					
+				});
+			});
+		}
 
- 
+
+	});
+// }
+	return res.redirect('bookslot.html');
+})
+
+
+
+app.get("/login", (req, res) => {
+
+
+
+
+	res.redirect("login.html");
+})
+
+
+
+// app.get("/nitin", (req, res) => {
+// 	let arr = ["10:00 - 10:30", "10:30 - 11:00", "11:00 - 11:30", "11:30 - 12:00", "12:00 - 12:30", "12:30 - 1:00", "01:00 - 01:30", "01:30 - 02:00", "02:00 - 02:30", "02:30 - 03:00", "03:00 - 03:30", "03:30 - 04:00", "04:00 - 04:30","04:30 - 05:00"]
+
+// 	for (i = 1; i <= 30; i++) {
+// 		for (j = 0; j < 14; j++) {
+// 			data = {
+// 				"date": i.toString(),
+// 				"count": 10,
+// 				"timeSlot": arr[j],
+// 			}
+// 			db.collection('vaccine').insertOne(data, function (err, collection) {
+// 				if (err) throw err;
+
+
+// 			});
+// 		}
+// 	}
+
+
+
+// 	res.send("loginhtml");
+// })
+
+app.post("/login", async (req, res) => {
+	console.log("Entry 1");
+
+
+	try {
+		const phone = req.body.phone;
+		const password = req.body.password;
+		const userPhoneNumber = await db.collection("users").findOne({ phone: phone });
+
+		console.log(phone, password, userPhoneNumber);
+
+		if (userPhoneNumber.password === password) {
+			userId = phone;
+			res.redirect("bookslot.html");
+		}
+		else {
+			res.send("Inavlid Login Details");
+		}
+
+	}
+	catch (err) {
+		console.log(err);
+	}
+
+
+})
+
+
+
+app.get("/getvaccinedozes", function (req, res) {
+	let userDocument;
+	db.collection("vaccinedozes").findOne({ userId: userId }, function (err, result) {
+		if (err) throw err;
+		userDocument = result;
+		console.log(userDocument);
+		if(userDocument){
+
+			res.send(userDocument);
+		}else{
+			res.send({firstDoze:false})
+		}
+	});
 })
 
 
 
 
-app.get('/',function(req,res){
-res.set({
-	'Access-control-Allow-Origin': '*'
+app.get('/', function (req, res) {
+	res.set({
+		'Access-control-Allow-Origin': '*'
 	});
-return res.redirect('index.html');
+	return res.redirect('index.html');
 }).listen(3001)
+
 
 
 console.log("server listening at port 3001");
--- public/bookslot.js
+++ public/bookslot.js
@@ -4,31 +4,53 @@ const timeSlot = document.querySelector(".timeSlot")
 
 // getting new date, current year and month
 let date = new Date(),
-currYear = date.getFullYear(),
-currMonth = date.getMonth();
+  currYear = date.getFullYear(),
+  currMonth = date.getMonth();
 
 
 const renderCalendar = () => {
-    
-    
-    let liTag = "";
-    
-    for (let i = 1; i <= 30; i++) { // creating li of all days of current month
-        // adding active class to li if the current day, month, and year matched
-        
-        liTag += `<li data-toggle="modal" data-target="#exampleModal" onclick = "openSlot(${i})" class="currDate${i}">${i}</li>`;
-    }
-    daysTag.innerHTML = liTag;
+
+
+  let liTag = "";
+
+  for (let i = 1; i <= 30; i++) { // creating li of all days of current month
+    // adding active class to li if the current day, month, and year matched
+
+    liTag += `<li data-toggle="modal" data-target="#exampleModal" onclick = "openSlot(${i})" class="currDate${i}">${i}</li>`;
+  }
+  daysTag.innerHTML = liTag;
 }
 
 renderCalendar();
 
 
+const options = { method: 'GET' };
 
-function funcc( curr)
-{
-   
-}
+fetch('http://localhost:3001/getvaccinedozes', options)
+  .then(response => response.json())
+  .then(response => {
+    console.log(response)
+
+    if (response.firstDoze) {
+      console.log("h")
+      if (response.firstDoze == true) {
+        document.getElementById("firstDoze").checked = true;
+        document.getElementById("firstDoze").disabled = true;
+      }
+      if (response.secondDoze == true) {
+        document.getElementById("secondDoze").checked = true;
+        document.getElementById("secondDoze").disabled = true;
+      }
+    }
+    else {
+      console.log("Hello")
+      document.getElementById("secondDoze").disabled = true;
+    }
+
+  })
+  .catch(err => console.error(err));
+
+
 
 var modal = document.getElementById("myModal");
 var modal2 = document.getElementById("myModal2");
@@ -42,32 +64,30 @@ var span2 = document.getElementsByClassName("close2")[0];
 // When the user clicks on the button, open the modal
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
-span2.onclick = function() {
+span2.onclick = function () {
   modal2.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
 
-function openSlot(curr)
-{
+function openSlot(curr) {
   slotHeading.innerHTML = `<h4> Book Your Slot for ${curr} June</h4>`
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
-function openDoze(curr)
-{
+function openDoze(curr) {
   modal2.style.display = "block";
-  if(curr==1){
+  if (curr == 1) {
     let val = "10:00 - 10:30"
     timeSlot.innerHTML = `<h6>Book ${val}</h6>`
-    document.getElementById("timeSlot").value =val;
+    document.getElementById("timeSlot").value = val;
   }
 }
 
